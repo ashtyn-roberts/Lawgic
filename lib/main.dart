7b3b49db3a6401b5870b8d678738d89521a27913
Lwachterr <lwacht3@lsu.edu>
2025-10-08 22:11:09 -0500

    base map screen, no API

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class _HomeScreenState extends State<HomeScreen> {
     const HomeTab(),
     const SearchTab(),
     const ProfileTab(),
+    const MapTab(),
   ];
 
   void _onItemTapped(int index) {
@@ -66,6 +67,9 @@ class _HomeScreenState extends State<HomeScreen> {
             icon: Icon(Icons.person),
             label: 'Profile',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',)
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.deepPurple,
@@ -258,5 +262,72 @@ class ProfileTab extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class MapTab extends StatelessWidget {
+  const MapTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        
+        children: [
+          Text(
+            'Voting Locations',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 32),
+          Container(
+            width:400,
+            height:350,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              border:Border.all(color: Colors.black, width: 2)
+            ),),
+          SizedBox(height: 20),
+          Text(
+            'Welcome *Username*',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'Your voting ward: xx/xxx',
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.grey,
+            ),
+          ),
+          Text(
+            'Location: *Building Name*',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey,
+            ),
+          ),
+          Text(
+            'Address: *Building Address*',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 32),
+          Text(
+            'Open in Google Maps',
+            style: TextStyle(
+              fontSize : 24,
+              color: Colors.red[600],
+            ))
+        ],
+      ),
+    ); 
   }
 }
